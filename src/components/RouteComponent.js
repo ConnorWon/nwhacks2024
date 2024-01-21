4a895dd4e2acd2c0bf1d5208c8ebf041d455d673
Ryan Yae <ryancyae@gmail.com>
2024-01-21 08:06:39 -0800

    fixed

--- src/components/RouteComponent.js
+++ src/components/RouteComponent.js
@@ -1,29 +1,37 @@
 // RouteComponent.js
-import React from "react";
-import { useQuery } from "react-query";
-import { Polyline } from "react-leaflet";
+// import React from "react";
+// import { useQuery } from "react-query";
+// import { Polyline } from "react-leaflet";
 
-const fetchRoute = async (start, end) => {
+const fetchRoute = async (cord) => {
   const apiKey =
     "pk.eyJ1IjoiY3dvbiIsImEiOiJjbHJtcDhucDQxMjY4MmtrZHA4cjY4Yzk0In0.pUcXee7umuJcGYPfes9kSQ"; // Replace with your Mapbox API key
   const response = await fetch(
-    "https://api.mapbox.com/directions/v5/mapbox/walking?access_token=pk.eyJ1IjoiY3dvbiIsImEiOiJjbHJtcDhucDQxMjY4MmtrZHA4cjY4Yzk0In0.pUcXee7umuJcGYPfes9kSQ",
-    {
-      body: "coordinates=49.2827,-123.1207;49.26411700132184,-123.21537018955787",
-    }
+    `https://api.mapbox.com/directions/v5/mapbox/walking/${cord.map((item, index) => {return(index === 0 ? item[0]+"%2C"+item[1] : "%3b"+item[0]+"%2C"+item[1]);})}?alternatives=true&continue_straight=true&geometries=geojson&language=en&overview=full&steps=true&access_token=${apiKey}`
   );
   const data = await response.json();
-  return data.routes[0].geometry.coordinates;
+  console.log(data);
+  console.log(data.routes[0].geometry.coordinates);
+  const flipData = [];
+  data.routes[0].geometry.coordinates.map((item, index) => {
+    return flipData[index] = [item[1], item[0]]
+  })
+  return flipData;
 };
 
-const RouteComponent = ({ start, end }) => {
-  const { data: routeCoordinates } = useQuery(["route", start, end], () =>
-    fetchRoute(start, end)
-  );
+const RouteComponent = (cord) => {
+  // const { data: routeCoordinates } = useQuery(["route", start, end], () =>
+  //   fetchRoute(start, end)
+  // );
 
-  return routeCoordinates ? (
-    <Polyline positions={routeCoordinates} color="black" />
-  ) : null;
+  // const [routeCoordinates, setRouteCoordinates] = useState(null);
+  // useEffect(() => {
+  //   setRouteCoordinates(fetchRoute(start, end));
+  // }, []);
+
+  // return routeCoordinates ? (
+  //   <Polyline positions={routeCoordinates} color="black" />
+  // ) : null;
 };
 
 export default RouteComponent;
